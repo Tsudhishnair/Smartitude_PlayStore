cec5201ae6c8a181d741282d6a70d3fff0965c54
Sudhish Nair <sudhishnair53@gmail.com>
2019-01-30 16:30:45 +0530

    changes login

--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -1,26 +1,37 @@
-import React, { Component } from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Login from '../../views/Login/Login';
-import GridItem from "components/Grid/GridItem.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
 
-class Log extends Component {
-    render() {
-        return (
-            <div style={{background:"linear-gradient(60deg, #ffa726, #fb8c00)",height:'auto'}}>
-                <GridContainer fullHeight>
-                    <GridItem md={6}
-                        justify="center"
-                        alignItems="center"
-                    >
-                       {/* <img src={new_logo} alt="logo"/>; */}
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={6}>
-                        <Login />
-                    </GridItem>
-                </GridContainer>
-            </div>
-        )
-    }
+
+const styles = theme => ({
+  root: {
+    background:"linear-gradient(80deg,#ffa726,#fb8c00)",
+    height:"100vh",
+  },
+ 
+});
+
+function CenteredGrid(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item md={6}>
+          
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Login/>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Log;
+CenteredGrid.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CenteredGrid);
