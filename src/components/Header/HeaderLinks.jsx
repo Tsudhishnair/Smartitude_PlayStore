78ec39d461793c5e6e8ca6b1e0993cd4635a9c41
Sudhish Nair <sudhishnair53@gmail.com>
2019-01-30 14:32:00 +0530

    searchbar removed

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -41,13 +41,13 @@ class HeaderLinks extends React.Component {
     const { open } = this.state;
     return (
       <div>
-        <div className={classes.searchWrapper}>
+        {/* <div className={classes.searchWrapper}>
           <CustomInput
             formControlProps={{
               className: classes.margin + " " + classes.search
             }}
             inputProps={{
-              placeholder: "Search",
+              placeholder: "Search",  
               inputProps: {
                 "aria-label": "Search"
               }
@@ -56,7 +56,7 @@ class HeaderLinks extends React.Component {
           <Button color="white" aria-label="edit" justIcon round>
             <Search />
           </Button>
-        </div>
+        </div> */}
         <Button
           color={window.innerWidth > 959 ? "transparent" : "white"}
           justIcon={window.innerWidth > 959}
--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -1,11 +1,25 @@
-import React,{Component} from "react";
-class Login extends Component{
-render()
-{
-    return{
-        
+import React, { Component } from "react";
+import Login from '../../views/Login/Login';
+import logo from "../../assets/img/faces"
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <GridConatiner>
+                    <GridItem md={6}
+                    justify="center"
+                    alignItems="center"
+                    >
+                        {new_logo}
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={6}>
+                        <Login />
+                    </GridItem>
+                </GridConatiner>
+            </div>
+
+        )
     }
-}
 }
 
 export default Login;
