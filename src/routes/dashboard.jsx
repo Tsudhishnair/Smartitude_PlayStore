9c27d90f637d36642f3d4d28a805a7c252f4e534
mebinjohnson <mebinjohnson@gmail.com>
2019-01-30 10:08:59 +0530

    Init

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,83 @@
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+// import ContentPaste from "@material-ui/icons/ContentPaste";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+import LogOutIcon from "@material-ui/icons/ExitToApp";
+import BarChart from "@material-ui/icons/BarChart"
+import NotificationIcon from "@material-ui/icons/QuestionAnswer"
+// core components/views
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import UserProfile from "views/UserProfile/UserProfile.jsx";
+import TableList from "views/TableList/TableList.jsx";
+
+import Typography from "views/Typography/Typography.jsx";
+import Icons from "views/Icons/Icons.jsx";
+import Maps from "views/Maps/Maps.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    sidebarName: "Dashboard",
+    navbarName: "Material Dashboard",
+    icon: Dashboard,
+    component: DashboardPage
+  },
+  {
+    path: "/user",
+    sidebarName: "Results",
+    navbarName: "Results",
+    icon: BarChart,
+    component: UserProfile
+  },
+  {
+    path: "/notifications",
+    sidebarName: "Notifications",
+    navbarName: "Notifications",
+    icon: NotificationIcon,
+    component: TableList
+  },
+  // {
+  //   path: "/typography",
+  //   sidebarName: "Typography",
+  //   navbarName: "Typography",
+  //   icon: LibraryBooks,
+  //   component: Typography
+  // },
+  // {
+  //   path: "/icons",
+  //   sidebarName: "Icons",
+  //   navbarName: "Icons",
+  //   icon: BubbleChart,
+  //   component: Icons
+  // },
+  // {
+  //   path: "/maps",
+  //   sidebarName: "Maps",
+  //   navbarName: "Map",
+  //   icon: LocationOn,
+  //   component: Maps
+  // },
+  // {
+  //   path: "/notifications",
+  //   sidebarName: "Notifications",
+  //   navbarName: "Notifications",
+  //   icon: Notifications,
+  //   component: NotificationsPage
+  // },
+  {
+    path: "/upgrade-to-pro",
+    sidebarName: "Log Out",
+    navbarName: "Upgrade To PRO",
+    icon: LogOutIcon,
+    component: UpgradeToPro
+  },
+  { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
+];
+
+export default dashboardRoutes;
