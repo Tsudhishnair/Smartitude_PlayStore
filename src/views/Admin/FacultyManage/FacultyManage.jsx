bb2c2d6c286e61282e08cd3f722f2b010c6baa2a
mebinjohnson <mebinjohnson@gmail.com>
2019-02-01 11:49:36 +0530

    asf

--- src/views/Admin/FacultyManage/FacultyManage.jsx
+++ src/views/Admin/FacultyManage/FacultyManage.jsx
@@ -21,79 +21,79 @@ import { lighten } from '@material-ui/core/styles/colorManipulator';
 let counter = 0;
 function createData(name, calories, fat, carbs, protein) {
   counter += 1;
-return { id: counter, name, calories, fat, carbs, protein };
+  return { id: counter, name, calories, fat, carbs, protein };
 }
 function desc(a, b, orderBy) {
-if (b[orderBy] < a[orderBy]) {
-return -1;
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
-if (b[orderBy] > a[orderBy]) {
-return 1;
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
   }
-return 0;
+  return 0;
 }
 function stableSort(array, cmp) {
-const stabilizedThis = array.map((el, index) => [el, index]);
-stabilizedThis.sort((a, b) => {
-const order = cmp(a[0], b[0]);
-if (order !== 0) return order;
-return a[1] - b[1];
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
   });
-return stabilizedThis.map(el => el[0]);
+  return stabilizedThis.map(el => el[0]);
 }
 function getSorting(order, orderBy) {
-return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
 }
 const rows = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+  { id: 'name', numeric: false, disablePadding: true, label: 'Faculty Name' },
+  { id: 'calories', numeric: true, disablePadding: false, label: 'Role' },
+  { id: 'fat', numeric: true, disablePadding: false, label: 'Category Assigned' },
+  { id: 'carbs', numeric: true, disablePadding: false, label: 'Questions Submitted' },
   { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
 ];
 class EnhancedTableHead extends React.Component {
-createSortHandler = property => event => {
-this.props.onRequestSort(event, property);
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
   };
-render() {
-const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
-return (
-<TableHead>
-<TableRow>
-<TableCell padding="checkbox">
-<Checkbox
+  render() {
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
               indeterminate={numSelected > 0 && numSelected < rowCount}
               checked={numSelected === rowCount}
               onChange={onSelectAllClick}
-/>
-</TableCell>
+            />
+          </TableCell>
           {rows.map(
-row => (
-<TableCell
+            row => (
+              <TableCell
                 key={row.id}
                 align={row.numeric ? 'right' : 'left'}
                 padding={row.disablePadding ? 'none' : 'default'}
                 sortDirection={orderBy === row.id ? order : false}
->
-<Tooltip
+              >
+                <Tooltip
                   title="Sort"
                   placement={row.numeric ? 'bottom-end' : 'bottom-start'}
                   enterDelay={300}
->
-<TableSortLabel
+                >
+                  <TableSortLabel
                     active={orderBy === row.id}
                     direction={order}
                     onClick={this.createSortHandler(row.id)}
->
+                  >
                     {row.label}
-</TableSortLabel>
-</Tooltip>
-</TableCell>
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
             ),
-this,
+            this,
           )}
-</TableRow>
-</TableHead>
+        </TableRow>
+      </TableHead>
     );
   }
 }
@@ -110,15 +110,15 @@ const toolbarStyles = theme => ({
     paddingRight: theme.spacing.unit,
   },
   highlight:
-theme.palette.type === 'light'
-? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-: {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
+    theme.palette.type === 'light'
+      ? {
+        color: theme.palette.secondary.main,
+        backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+      }
+      : {
+        color: theme.palette.text.primary,
+        backgroundColor: theme.palette.secondary.dark,
+      },
   spacer: {
     flex: '1 1 100%',
   },
@@ -130,41 +130,41 @@ theme.palette.type === 'light'
   },
 });
 let EnhancedTableToolbar = props => {
-const { numSelected, classes } = props;
-return (
-<Toolbar
+  const { numSelected, classes } = props;
+  return (
+    <Toolbar
       className={classNames(classes.root, {
         [classes.highlight]: numSelected > 0,
       })}
->
-<div className={classes.title}>
+    >
+      <div className={classes.title}>
         {numSelected > 0 ? (
-<Typography color="inherit" variant="subtitle1">
+          <Typography color="inherit" variant="subtitle1">
             {numSelected} selected
 </Typography>
         ) : (
-<Typography variant="h6" id="tableTitle">
-            Nutrition
+            <Typography variant="h6" id="tableTitle">
+              Faculty
 </Typography>
-        )}
-</div>
-<div className={classes.spacer} />
-<div className={classes.actions}>
+          )}
+      </div>
+      <div className={classes.spacer} />
+      <div className={classes.actions}>
         {numSelected > 0 ? (
-<Tooltip title="Delete">
-<IconButton aria-label="Delete">
-<DeleteIcon />
-</IconButton>
-</Tooltip>
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
         ) : (
-<Tooltip title="Filter list">
-<IconButton aria-label="Filter list">
-<FilterListIcon />
-</IconButton>
-</Tooltip>
-        )}
-</div>
-</Toolbar>
+            <Tooltip title="Filter list">
+              <IconButton aria-label="Filter list">
+                <FilterListIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+      </div>
+    </Toolbar>
   );
 };
 EnhancedTableToolbar.propTypes = {
@@ -178,7 +178,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
   },
   table: {
-    minWidth: 1020,
+    minWidth: 900,
   },
   tableWrapper: {
     overflowX: 'auto',
@@ -190,43 +190,43 @@ class EnhancedTable extends React.Component {
     orderBy: 'calories',
     selected: [],
     data: [
-createData('Cupcake', 305, 3.7, 67, 4.3),
-createData('Donut', 452, 25.0, 51, 4.9),
-createData('Eclair', 262, 16.0, 24, 6.0),
-createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-createData('Gingerbread', 356, 16.0, 49, 3.9),
-createData('Honeycomb', 408, 3.2, 87, 6.5),
-createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-createData('Jelly Bean', 375, 0.0, 94, 0.0),
-createData('KitKat', 518, 26.0, 65, 7.0),
-createData('Lollipop', 392, 0.2, 98, 0.0),
-createData('Marshmallow', 318, 0, 81, 2.0),
-createData('Nougat', 360, 19.0, 9, 37.0),
-createData('Oreo', 437, 18.0, 63, 4.0),
+      createData('Cupcake', 'Faculty', 3.7, 67, 4.3),
+      createData('Donut', 'Incharge', 25.0, 51, 4.9),
+      createData('Eclair', 'Faculty', 16.0, 24, 6.0),
+      createData('Frozen yoghurt', 'Incharge', 6.0, 24, 4.0),
+      createData('Gingerbread', 'Incharge', 16.0, 49, 3.9),
+      createData('Honeycomb', 'Faculty', 3.2, 87, 6.5),
+      createData('Ice cream sandwich', 'Incharge', 9.0, 37, 4.3),
+      createData('Jelly Bean', 'Faculty', 0.0, 94, 0.0),
+      createData('KitKat', 'Faculty', 26.0, 65, 7.0),
+      createData('Lollipop', 'Faculty', 0.2, 98, 0.0),
+      createData('Marshmallow', 'Incharge', 0, 81, 2.0),
+      createData('Nougat', 'Faculty', 19.0, 9, 37.0),
+      createData('Oreo', 'Incharge', 18.0, 63, 4.0),
     ],
     page: 0,
-    rowsPerPage: 5,
+    rowsPerPage: 10,
   };
-handleRequestSort = (event, property) => {
-const orderBy = property;
-let order = 'desc';
-if (this.state.orderBy === property && this.state.order === 'desc') {
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+    if (this.state.orderBy === property && this.state.order === 'desc') {
       order = 'asc';
     }
-this.setState({ order, orderBy });
+    this.setState({ order, orderBy });
   };
-handleSelectAllClick = event => {
-if (event.target.checked) {
-this.setState(state => ({ selected: state.data.map(n => n.id) }));
-return;
+  handleSelectAllClick = event => {
+    if (event.target.checked) {
+      this.setState(state => ({ selected: state.data.map(n => n.id) }));
+      return;
     }
-this.setState({ selected: [] });
+    this.setState({ selected: [] });
   };
-handleClick = (event, id) => {
-const { selected } = this.state;
-const selectedIndex = selected.indexOf(id);
-let newSelected = [];
-if (selectedIndex === -1) {
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+    if (selectedIndex === -1) {
       newSelected = newSelected.concat(selected, id);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
@@ -234,43 +234,43 @@ if (selectedIndex === -1) {
       newSelected = newSelected.concat(selected.slice(0, -1));
     } else if (selectedIndex > 0) {
       newSelected = newSelected.concat(
-selected.slice(0, selectedIndex),
-selected.slice(selectedIndex + 1),
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
       );
     }
-this.setState({ selected: newSelected });
+    this.setState({ selected: newSelected });
   };
-handleChangePage = (event, page) => {
-this.setState({ page });
+  handleChangePage = (event, page) => {
+    this.setState({ page });
   };
-handleChangeRowsPerPage = event => {
-this.setState({ rowsPerPage: event.target.value });
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
   };
-isSelected = id => this.state.selected.indexOf(id) !== -1;
-render() {
-const { classes } = this.props;
-const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
-const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
-return (
-<Paper className={classes.root}>
-<EnhancedTableToolbar numSelected={selected.length} />
-<div className={classes.tableWrapper}>
-<Table className={classes.table} aria-labelledby="tableTitle">
-<EnhancedTableHead
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+  render() {
+    const { classes } = this.props;
+    const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+    return (
+      <Paper className={classes.root}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <EnhancedTableHead
               numSelected={selected.length}
               order={order}
               orderBy={orderBy}
               onSelectAllClick={this.handleSelectAllClick}
               onRequestSort={this.handleRequestSort}
               rowCount={data.length}
-/>
-<TableBody>
+            />
+            <TableBody>
               {stableSort(data, getSorting(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map(n => {
-const isSelected = this.isSelected(n.id);
-return (
-<TableRow
+                  const isSelected = this.isSelected(n.id);
+                  return (
+                    <TableRow
                       hover
                       onClick={event => this.handleClick(event, n.id)}
                       role="checkbox"
@@ -278,44 +278,44 @@ return (
                       tabIndex={-1}
                       key={n.id}
                       selected={isSelected}
->
-<TableCell padding="checkbox">
-<Checkbox checked={isSelected} />
-</TableCell>
-<TableCell component="th" scope="row" padding="none">
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox checked={isSelected} />
+                      </TableCell>
+                      <TableCell component="th" scope="row" padding="none">
                         {n.name}
-</TableCell>
-<TableCell align="right">{n.calories}</TableCell>
-<TableCell align="right">{n.fat}</TableCell>
-<TableCell align="right">{n.carbs}</TableCell>
-<TableCell align="right">{n.protein}</TableCell>
-</TableRow>
+                      </TableCell>
+                      <TableCell align="right">{n.calories}</TableCell>
+                      <TableCell align="right">{n.fat}</TableCell>
+                      <TableCell align="right">{n.carbs}</TableCell>
+                      <TableCell align="right">{n.protein}</TableCell>
+                    </TableRow>
                   );
                 })}
               {emptyRows > 0 && (
-<TableRow style={{ height: 49 * emptyRows }}>
-<TableCell colSpan={6} />
-</TableRow>
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
               )}
-</TableBody>
-</Table>
-</div>
-<TablePagination
+            </TableBody>
+          </Table>
+        </div>
+        <TablePagination
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
           count={data.length}
           rowsPerPage={rowsPerPage}
           page={page}
           backIconButtonProps={{
-'aria-label': 'Previous Page',
+            'aria-label': 'Previous Page',
           }}
           nextIconButtonProps={{
-'aria-label': 'Next Page',
+            'aria-label': 'Next Page',
           }}
           onChangePage={this.handleChangePage}
           onChangeRowsPerPage={this.handleChangeRowsPerPage}
-/>
-</Paper>
+        />
+      </Paper>
     );
   }
 }
