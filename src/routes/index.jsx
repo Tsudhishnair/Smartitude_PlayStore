f67d1091099c27881330e417f9c560550ce7afb6
mebinjohnson <mebinjohnson@gmail.com>
2019-02-13 11:03:58 +0530

    Restructure

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,8 @@
 import StudentDash from "../layouts/StudentPanel/StudentPanel.jsx";
 import Login from "../layouts/Login/Login.jsx"
+import Landing from "../layouts/Landing/Landing"
 import AdminDash from "../layouts/AdminPanel/AdminPanel.jsx"
 
-const indexRoutes = [{path: "/login", component: Login},{path: "/", component: AdminDash},{ path: "/student", component: StudentDash },];
+const indexRoutes = [{path: "/home", component: Landing},{path: "/login", component: Login},{path: "/admin/", component: AdminDash},{ path: "/student/", component: StudentDash },];
 
 export default indexRoutes;
