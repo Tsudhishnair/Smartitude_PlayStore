d9fd8ed7f3fb612b3516dadfb9477855677ee8f5
Prejith Premkumar <prajithpremg@gmail.com>
2019-02-14 14:03:56 +0530

    fix login

--- src/index.js
+++ src/index.js
@@ -5,26 +5,44 @@ import { Router, Route, Switch } from "react-router-dom";
 
 import "assets/css/material-dashboard-react.css?v=1.5.0";
 import indexRoutes from "routes/index.jsx";
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { orange700 } from "material-ui/styles/colors";
 
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-});
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { createHttpLink } from "apollo-link-http";
 
 const theme = createMuiTheme({
   palette: {
     primary: { main: orange700 }, // Purple and green play nicely together.
-    secondary: { main: orange700 }, // This is just green.A700 as hex.
+    secondary: { main: orange700 } // This is just green.A700 as hex.
   },
-  typography: { useNextVariants: true },
+  typography: { useNextVariants: true }
 });
 
-
 const hist = createBrowserHistory();
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
