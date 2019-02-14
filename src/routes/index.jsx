e1610eb886529516046987ef64b19bf569277416
Prejith Premkumar <prajithpremg@gmail.com>
2019-02-14 15:05:39 +0530

    bleh

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import FacultyLogin from "../layouts/Login/FacultyLogin.jsx";
 import Landing from "../layouts/Landing/Landing";
 import AdminDash from "../layouts/AdminPanel/AdminPanel.jsx";
 
-const indexRoutes = [  
+const indexRoutes = [
   { path: "/admin/login", component: AdminLogin },
   { path: "/fac/login", component: FacultyLogin },
   { path: "/stud/login", component: StudentLogin },
