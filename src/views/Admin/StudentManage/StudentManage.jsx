b2f70b01f044259ac34f60ca0d2e848c194004b8
Prejith Premkumar <prajithpremg@gmail.com>
2019-02-17 08:24:38 +0530

    fix bugs

--- src/views/Admin/StudentManage/StudentManage.jsx
+++ src/views/Admin/StudentManage/StudentManage.jsx
@@ -18,72 +18,72 @@ import gql from "graphql-tag";
 
 import { ApolloProvider } from "react-apollo";
 
-
 const styles = theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 6,
-  },
+    width: "100%",
+    marginTop: theme.spacing.unit * 6
+  }
 });
 class Dashboard extends React.Component {
-
   render() {
     const { classes } = this.props;
-    const header1 = 'Student';
-    const header2 = 'Add a new student';
-    const columns = [{
-      name: "Name",
-      options: {
-        filter: false,
-        sort: true,
+    const header1 = "Student";
+    const header2 = "Add a new student";
+    const columns = [
+      {
+        name: "Name",
+        options: {
+          filter: false,
+          sort: true
+        }
+      },
+      {
+        name: "Username",
+        options: {
+          filter: false,
+          sort: true
+        }
+      },
+      {
+        name: "Department",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "Batch",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "Phone",
+        options: {
+          filter: false,
+          sort: false
+        }
+      },
+      {
+        name: "Rank",
+        options: {
+          filter: false,
+          sort: true
+        }
+      },
+      {
+        name: "Score",
+        options: {
+          filter: false,
+          sort: true
+        }
       }
-    },
-    {
-      name: "Username",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "Department",
-      options: {
-        filter: true,
-        sort: true,
-      }
-    },
-    {
-      name: "Batch",
-      options: {
-        filter: true,
-        sort: true,
-      }
-    },
-    {
-      name: "Phone",
-      options: {
-        filter: false,
-        sort: false,
-      }
-    },
-    {
-      name: "Rank",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "Score",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },];
+    ];
 
     const dataList = gql`
-    {
-      student{
+      {
+        student {
           _id
           username
           name
@@ -94,8 +94,8 @@ class Dashboard extends React.Component {
           batch
           attemptedAdminQuizzes
           attemptedCustomQuizzes
+        }
       }
-    }
     `;
     // const data = [
     //   // ["Joe James", "uxxxxx", "CS", "2015", "12345678", "2", "1.0"],
@@ -113,58 +113,65 @@ class Dashboard extends React.Component {
     //   // ["George Hoj", "uxxxxx", "CS", "2015", "12345678", "3", "6.1"],
     //   // ["Ashiq Hassan", "uxxxxx", "IT", "2015", "12345678", "1", "8.0"],
 
-
     // ];
 
     const options = {
-      filterType: 'checkbox',
+      filterType: "checkbox",
       rowsPerPage: 20,
       elevation: 0,
       rowsPerPageOptions: [20, 30, 100, 200],
 
       onRowClick: (rowData, rowState) => {
         console.log(rowData, rowState);
-        this.child.handleClickOpen(rowData)
-      },
+        this.child.handleClickOpen(rowData);
+      }
     };
 
     return (
-        <div>
-          <Query query={dataList}>
-        {({ data, loading, error }) => {
-        return(
-          console.log({ loading })
-   ) 
-         if(!loading) return (
-          console.log({ data })
-        );
-
-        }}
+      <div>
+        <Query query={dataList}>
+          {({ data, loading, error }) => {
+            // return console.log({ loading });
+            // if (!loading) return console.log({ data });
+            console.log("data", data, "loading", loading, "error", error);
+            return <div />;
+          }}
         </Query>
-            <TableDialog onRef={ref => (this.child = ref)} />
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={12}>
-                <Exp headers={header1} header={header2} Footer1={"Cancel"} Footer2={"Assign"} directingValue={"4"} />
-                <Exp headers={"Multiple Student"} header={"Add groups of students"} Footer1={"Cancel"} Footer2={"Assign"} directingValue={"3"} />
-              </GridItem>
-            </GridContainer>
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={12}>
-                <Card className={classes.root}>
-                  <CardHeader color="warning">
-                    <h4 className={classes.cardTitleWhite}>Student List</h4>
-                  </CardHeader>
-                  <MUIDataTable
-                    title={""}
-                    // data={data}
-                    columns={columns}
-                    options={options}
-                  />
-                </Card>
-              </GridItem >
-            </GridContainer >
-          </div>
-
+        <TableDialog onRef={ref => (this.child = ref)} />
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Exp
+              headers={header1}
+              header={header2}
+              Footer1={"Cancel"}
+              Footer2={"Assign"}
+              directingValue={"4"}
+            />
+            <Exp
+              headers={"Multiple Student"}
+              header={"Add groups of students"}
+              Footer1={"Cancel"}
+              Footer2={"Assign"}
+              directingValue={"3"}
+            />
+          </GridItem>
+        </GridContainer>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card className={classes.root}>
+              <CardHeader color="warning">
+                <h4 className={classes.cardTitleWhite}>Student List</h4>
+              </CardHeader>
+              <MUIDataTable
+                title={""}
+                // data={data}
+                columns={columns}
+                options={options}
+              />
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
     );
   }
 }
