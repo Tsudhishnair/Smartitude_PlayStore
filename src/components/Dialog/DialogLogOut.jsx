5c848a9da423d65ba332101b0527c3e041d7e469
mebinjohnson <mebinjohnson@gmail.com>
2019-02-19 09:27:39 +0530

    Log Out

---
+++ src/components/Dialog/DialogLogOut.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+
+import Spacing from "../Spacing/Spacing";
+const styles = theme => ({
+  appBar: {
+    position: "relative"
+  },
+  flex: {
+    flex: 1
+  },
+  formControl: {
+    margin: 0,
+    padding: theme.spacing.unit * 10,
+    fullWidth: true,
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 2,
+    backgroundColor: "#9ee",
+    wrap: "nowrap"
+  },
+  elementPadding: {
+    padding: "15px",
+    marginBottom: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 10
+  },
+  container: {
+    display: "flex",
+
+    flexGrow: 1
+  },
+  root: {
+    flexGrow: 1,
+    marginLeft: 10
+  },
+  button: {
+    margin: theme.spacing.unit * 4
+  }
+});
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
+
+class DialogLogOut extends React.Component {
+  state = {
+    open: true,
+    delopen: false
+  };
+  // componentDidMount() {
+  //   this.props.onRef(this);
+  // }
+  // componentWillUnmount() {
+  //   this.props.onRef(undefined);
+  // }
+  handleDeleteClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDeleteClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleDeleteClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Are you sure you want to log out?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              This action once done cannot be undone. Please continue with
+              caution.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleDeleteClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleDeleteClose} color="primary" autoFocus>
+              Log Out
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+DialogLogOut.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(DialogLogOut);
