429457299d447be4cbc5d317e4e9dfb037f64a44
Prejith Premkumar <prajithpremg@gmail.com>
2019-03-23 23:34:35 +0530

    add todo

--- src/Utils.js
+++ src/Utils.js
@@ -11,7 +11,6 @@ export const loginHandler = {
   },
   logout: () => {
     localStorage.removeItem("token");
-    localStorage.removeItem("userType");
   },
   userType: localStorage.getItem("userType")
 };
