3dc0d7d893dd15a27d69e2e34defcb74799ec77a
sudhish <sudhishnair53@gmail.com>
2019-04-30 10:27:19 +0530

    :sparkles: Quiz Answer Panel created

--- src/views/Student/QuizPanel/QuizAnwerPage.jsx
+++ src/views/Student/QuizPanel/QuizAnwerPage.jsx
@@ -26,7 +26,11 @@ import Spacing from "../../../components/Spacing/Spacing";
 import CardFooter from "../../../components/Card/CardFooter";
 import CardBody from "../../../components/Card/CardBody";
 import Timer from "react-compound-timer/build/components/Timer/Timer";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const styles = theme => ({
   root: {
     margin: theme.spacing.unit * 1,
@@ -62,167 +66,112 @@ class QuizAnswer extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        {/* <GridContainer>
+        <GridContainer>
           <GridItem>
             <Typography>
-              <h4>{quiz.name}</h4>
+              <h4>Quiz 1</h4>
             </Typography>
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={8}>
-            <Card>
-              <form>
-                <CardContent>
-                  <Typography>
-                    Question Number: <b>{this.getQuestionNumber()}</b>
-                  </Typography>
-                  <p>{this.state.fields.question}</p>
-                  <Divider />
-                </CardContent>
-                <CardBody>
-                  <FormControl
-                    component="fieldset"
-                    className={classes.formControl}
-                  >
-                    <FormLabel component="legend">
-                      Select your correct choice below
-                    </FormLabel>
-                    <RadioGroup
-                      aria-label="options"
-                      name="option"
-                      className={classes.group}
-                      value={this.state.markedOption}
-                      onChange={event =>
-                        this.handleChange(event, quiz.sections)
-                      }
-                    >
-                      <FormControlLabel
-                        value={1}
-                        control={<Radio />}
-                        label={this.state.fields.options["1"]}
-                      />
-                      <FormControlLabel
-                        value={2}
-                        control={<Radio />}
-                        label={this.state.fields.options["2"]}
-                      />
-                      <FormControlLabel
-                        value={3}
-                        control={<Radio />}
-                        label={this.state.fields.options["3"]}
-                      />
-                      <FormControlLabel
-                        value={4}
-                        control={<Radio />}
-                        label={this.state.fields.options["4"]}
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                  <Button
-                    variant="outlined"
-                    color={"secondary"}
-                    size={"small"}
-                    disabled={this.state.prevButton}
-                    className={classes.button}
-                    onClick={event =>
-                      this.handlePreviousClick(event, selectedSection)
-                    }
-                  >
-                    Previous
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    size={"small"}
-                    type={"reset"}
-                    className={classes.button}
-                    onClick={this.handleClearClick}
-                  >
-                    Clear Selection
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    size={"small"}
-                    disabled={this.state.nextButton}
-                    className={classes.button}
-                    onClick={event =>
-                      this.handleNextClick(event, selectedSection)
-                    }
-                  >
-                    Next
-                  </Button>
-                </CardFooter>
-              </form>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            <Card>
-              <CardContent>
-                <p>
-                  <Typography variant={"overline"}>Time Remaining:</Typography>
-                  <Typography variant={"h5"} className={classes.timer}>
-                    <b>
-                      <Timer
-                        initialTime={60000 * this.getTimeLimit(selectedSection)}
-                        direction="backward"
-                        onStop={this.handleSectionSubmit}
-                      >
-                        {(stop, getTimerState, getTime) => (
-                          <React.Fragment>
-                            <Timer.Minutes /> minutes <Timer.Seconds /> seconds
-                            {getTimerState}
-                          </React.Fragment>
-                        )}
-                      </Timer>
-                    </b>
-                  </Typography>
-                </p>
-                <Spacing />
-                <Typography variant={"overline"}>Questions:</Typography>
-                {this.generateQuestionJumpers(selectedSection, classes.fab)}
-              </CardContent>
-              <CardFooter>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  type={"submit"}
-                  size={"small"}
-                  fullWidth
-                  className={classes.button}
-                  onClick={() => this.handleSectionSubmit(quiz.sections)}
-                >
-                  {activeStep === steps.length - 1
-                    ? "Finish Quiz"
-                    : "Submit Section"}
-                </Button>
-              </CardFooter>
-            </Card>
           </GridItem>
         </GridContainer>
         <Spacing />
         <GridContainer>
           <GridItem xs={12} sm={12} md={12}>
             <Card>
-              <Stepper activeStep={activeStep} alternativeLabel>
-                {steps.map(label => {
-                  const props = {};
-                  const labelProps = {};
-                  return (
-                    <Step key={label} {...props}>
-                      <StepLabel {...labelProps}>{label}</StepLabel>
-                    </Step>
-                  );
-                })}
-              </Stepper>
+              <CardContent>
+                <p>
+                  <Typography variant={"overline"} />
+                  <Typography variant={"h5"}>
+                    <center>
+                      <p>Congratulation</p>
+                    </center>
+                  </Typography>
+                </p>
+              </CardContent>
+              <CardFooter>
+                <p>
+                  <strong>Score:</strong>25.5/50
+                </p>
+                <p>
+                  <strong>Negative Marks:</strong>24
+                </p>
+                <p>
+                  <strong>Time Taken:</strong>23.6 mins/30mins
+                </p>
+                <p>
+                  <strong>Current Rank:</strong>6th/150
+                </p>
+              </CardFooter>
             </Card>
           </GridItem>
-        </GridContainer> */}
-        
-        <p>hello asdbhjskldbgklhi</p>
+        </GridContainer>
+        <Spacing />
+
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <form>
+                    <CardContent>
+                      <Typography>
+                        Question Number: <b>1</b>
+                      </Typography>
+                      <p>
+                        A train travells at a spped of 120 km/hr. how long does
+                        it take to travel a distance of 3km?
+                      </p>
+                      <Divider />
+                    </CardContent>
+                    <CardBody>
+                      <FormControl
+                        component="fieldset"
+                        className={classes.formControl}
+                      >
+                        <FormLabel component="legend">
+                          Select your correct choice below
+                        </FormLabel>
+                        <RadioGroup
+                          aria-label="options"
+                          name="option"
+                          className={classes.group}
+                        >
+                          <FormControlLabel
+                            value={1}
+                            control={<Radio />}
+                            label={"1"}
+                          />
+                          <FormControlLabel
+                            value={2}
+                            control={<Radio />}
+                            label={"2"}
+                          />
+                          <FormControlLabel
+                            value={3}
+                            control={<Radio />}
+                            label={"3"}
+                          />
+                          <FormControlLabel
+                            value={4}
+                            control={<Radio />}
+                            label={"4"}
+                          />
+                        </RadioGroup>
+                      </FormControl>
+                    </CardBody>
+                    <Divider />
+                  </form>
+                </ExpansionPanelSummary>
+
+                <ExpansionPanelDetails>
+                  <p>
+                    <strong>Detailed Answer:</strong>
+                    <div>The has no fuel thus it travels zero km.</div>
+                  </p>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Card>
+          </GridItem>
+        </GridContainer>
       </div>
     );
   }
