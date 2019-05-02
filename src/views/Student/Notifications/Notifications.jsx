3b1782c71d5588261a0dfd43e7f2b9aa79cafacf
Prabha Shankar <shankar75031@gmail.com>
2019-05-02 21:54:02 +0530

    :sparkles: Student notifications.

--- src/views/Student/Notifications/Notifications.jsx
+++ src/views/Student/Notifications/Notifications.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable */
-import React from "react";
+import React, { Fragment } from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
-import AddAlert from "@material-ui/icons/AddAlert";
 // core components
-import GridItem from "components/Grid/GridItem.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import SnackbarContent from "components/Snackbar/SnackbarContent.jsx";
-import Snackbar from "components/Snackbar/Snackbar.jsx";
 import Card from "components/Card/Card.jsx";
+import gql from "graphql-tag";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
+import { Query } from "react-apollo";
+import { CircularProgress, Typography } from "@material-ui/core";
+import MUIDataTable from "mui-datatables";
 
 const styles = {
   cardCategoryWhite: {
@@ -44,18 +42,41 @@ const styles = {
   }
 };
 
-class Notifications extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tl: false,
-      tc: false,
-      tr: false,
-      bl: false,
-      bc: false,
-      br: false
-    };
+const columns = [
+  {
+    name: "Title",
+    options: {
+      filter: false,
+      sort: true
+    }
+  },
+  {
+    name: "Description",
+    options: {
+      filter: false,
+      sort: true
+    }
   }
+];
+
+// Query to fetch all messages sent to all the batchs
+const MESSAGES_FETCH_QUERY = gql`
+  {
+    batchMessages {
+      _id
+      title
+      description
+    }
+  }
+`;
+class Notifications extends React.Component {
+  options = {
+    filterType: "checkbox",
+    rowsPerPage: 20,
+    elevation: 0,
+    selectableRows: false,
+    rowsPerPageOptions: [20, 30, 100, 200]
+  };
   // to stop the warning of calling setState of unmounted component
   componentWillUnmount() {
     var id = window.setTimeout(null, 0);
@@ -63,228 +84,52 @@ class Notifications extends React.Component {
       window.clearTimeout(id);
     }
   }
-  showNotification(place) {
-    var x = [];
-    x[place] = true;
-    this.setState(x);
-    this.alertTimeout = setTimeout(
-      function() {
-        x[place] = false;
-        this.setState(x);
-      }.bind(this),
-      6000
-    );
+
+  constructor(props) {
+    super(props);
+    this.messageList = [];
   }
+
   render() {
     const { classes } = this.props;
     return (
       <Card>
         <CardHeader color="primary">
-          <h4 className={classes.cardTitleWhite}>Notifications</h4>
+          <h4 className={classes.cardTitleWhite}>Important</h4>
           <p className={classes.cardCategoryWhite}>
-            Handcrafted by our friends from{" "}
-            <a target="_blank" href="https://material-ui-next.com/">
-              Material UI
-            </a>{" "}
-            and styled by{" "}
-            <a target="_blank" href="">
-              Smartitude
-            </a>
+            See the important notifications for your batch below.
           </p>
         </CardHeader>
         <CardBody>
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <h5>Notifications Style</h5>
-              <br />
-              <SnackbarContent message={"This is a plain notification"} />
-              <SnackbarContent
-                message={"This is a notification with close button."}
-                close
-              />
-              <SnackbarContent
-                message={"This is a notification with close button and icon."}
-                close
-                icon={AddAlert}
-              />
-              <SnackbarContent
-                message={
-                  "This is a notification with close button and icon and have many lines. You can see that the icon and the close button are always vertically aligned. This is a beautiful notification. So you don't have to worry about the style."
-                }
-                close
-                icon={AddAlert}
-              />
-            </GridItem>
-            <GridItem xs={12} sm={12} md={6}>
-              <h5>Notifications States</h5>
-              <br />
-              <SnackbarContent
-                message={
-                  'INFO - This is a regular notification made with color="info"'
-                }
-                close
-                color="info"
-              />
-              <SnackbarContent
-                message={
-                  'SUCCESS - This is a regular notification made with color="success"'
-                }
-                close
-                color="success"
-              />
-              <SnackbarContent
-                message={
-                  'WARNING - This is a regular notification made with color="warning"'
-                }
-                close
-                color="warning"
-              />
-              <SnackbarContent
-                message={
-                  'DANGER - This is a regular notification made with color="danger"'
-                }
-                close
-                color="danger"
-              />
-              <SnackbarContent
-                message={
-                  'PRIMARY - This is a regular notification made with color="primary"'
-                }
-                close
-                color="primary"
-              />
-            </GridItem>
-          </GridContainer>
-          <br />
-          <br />
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={6} style={{ textAlign: "center" }}>
-              <h5>
-                Notifications Places
-                <small>Click to view notifications</small>
-              </h5>
-            </GridItem>
-          </GridContainer>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={10} lg={8}>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <Button
-                    fullWidth
-                    color="primary"
-                    onClick={() => this.showNotification("tl")}
-                  >
-                    Top Left
-                  </Button>
-                  <Snackbar
-                    place="tl"
-                    color="info"
-                    icon={AddAlert}
-                    message="Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer."
-                    open={this.state.tl}
-                    closeNotification={() => this.setState({ tl: false })}
-                    close
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <Button
-                    fullWidth
-                    color="primary"
-                    onClick={() => this.showNotification("tc")}
-                  >
-                    Top Center
-                  </Button>
-                  <Snackbar
-                    place="tc"
-                    color="info"
-                    icon={AddAlert}
-                    message="Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer."
-                    open={this.state.tc}
-                    closeNotification={() => this.setState({ tc: false })}
-                    close
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <Button
-                    fullWidth
-                    color="primary"
-                    onClick={() => this.showNotification("tr")}
-                  >
-                    Top Right
-                  </Button>
-                  <Snackbar
-                    place="tr"
-                    color="info"
-                    icon={AddAlert}
-                    message="Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer."
-                    open={this.state.tr}
-                    closeNotification={() => this.setState({ tr: false })}
-                    close
-                  />
-                </GridItem>
-              </GridContainer>
-            </GridItem>
-          </GridContainer>
-          <GridContainer justify={"center"}>
-            <GridItem xs={12} sm={12} md={10} lg={8}>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <Button
-                    fullWidth
-                    color="primary"
-                    onClick={() => this.showNotification("bl")}
-                  >
-                    Bottom Left
-                  </Button>
-                  <Snackbar
-                    place="bl"
-                    color="info"
-                    icon={AddAlert}
-                    message="Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer."
-                    open={this.state.bl}
-                    closeNotification={() => this.setState({ bl: false })}
-                    close
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <Button
-                    fullWidth
-                    color="primary"
-                    onClick={() => this.showNotification("bc")}
-                  >
-                    Bottom Center
-                  </Button>
-                  <Snackbar
-                    place="bc"
-                    color="info"
-                    icon={AddAlert}
-                    message="Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer."
-                    open={this.state.bc}
-                    closeNotification={() => this.setState({ bc: false })}
-                    close
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <Button
-                    fullWidth
-                    color="primary"
-                    onClick={() => this.showNotification("br")}
-                  >
-                    Bottom Right
-                  </Button>
-                  <Snackbar
-                    place="br"
-                    color="info"
-                    icon={AddAlert}
-                    message="Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer."
-                    open={this.state.br}
-                    closeNotification={() => this.setState({ br: false })}
-                    close
-                  />
-                </GridItem>
-              </GridContainer>
-            </GridItem>
-          </GridContainer>
+          <Query query={MESSAGES_FETCH_QUERY}>
+            {({ data, loading, error, refetch }) => {
+              this.reloadList = refetch;
+              if (loading) {
+                return <CircularProgress className={classes.progress}/>;
+              } else if (error) {
+                return <Typography>Error occured while fetching data!</Typography>;
+              } else {
+                this.messages = data.batchMessages;
+                this.messageList = data.batchMessages.map(message => {
+                  let messageDetails = [];
+                  messageDetails.push(message.title);
+                  messageDetails.push(message.description);
+                  return messageDetails;
+                });
+                return (
+                  <Fragment>
+                    <MUIDataTable
+                      title={""}
+                      data={this.messageList}
+                      columns={columns}
+                      options={this.options}
+                    />
+                  </Fragment>
+                );
+              }
+            }
+            }
+          </Query>
         </CardBody>
       </Card>
     );
