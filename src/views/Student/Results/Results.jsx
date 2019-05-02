13df7356cf68e2b4011eb6cc4199c6eefa5f0d82
Sudhish Nair <sudhishnair53@gmail.com>
2019-05-02 10:52:02 +0530

    :construction: Results Page

--- src/views/Student/Results/Results.jsx
+++ src/views/Student/Results/Results.jsx
@@ -16,100 +16,152 @@ import DateRange from "@material-ui/icons/DateRange";
 import LocalOffer from "@material-ui/icons/LocalOffer";
 import Icon from "@material-ui/core/Icon";
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
-
-function UserProfile(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card Green>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
-                <Icon>grade</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Current Score</p>
-              <h3 className={classes.cardTitle}>
-                4.9/10
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Danger>
-                  <Warning />
-                </Danger>
-                <a href="#pablo" onClick={e => e.preventDefault()}>
-                  Prepare More
-                </a>
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <Store />
-              </CardIcon>
-              <p className={classes.cardCategory}>Ranking</p>
-              <h3 className={classes.cardTitle}>
-                420th
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <DateRange />
-                Last 24 Hours
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="danger" stats icon>
-              <CardIcon color="danger">
-                <Icon>info_outline</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Attempted Quizzes</p>
-              <h3 className={classes.cardTitle}>12</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                Last one at 11/04/19
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
-                <LocalOffer />
-              </CardIcon>
-              <p className={classes.cardCategory}>Followers</p>
-              <h3 className={classes.cardTitle}>+245</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </GridContainer>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={12}>
-          <Card className={classes.root}>
-            <CardHeader color="warning">
-              <h4 className={classes.cardTitleWhite}>Attempted Quizzes</h4>
-            </CardHeader>
-          </Card>
-        </GridItem>
-      </GridContainer>
-    </div>
-  );
+import MUIDataTable from "mui-datatables";
+import CardBody from "../../../components/Card/CardBody";
+let datalist = [];
+datalist = [
+  [
+    "1",
+    "Quiz 1",
+    "Be the top in this quiz and get place in Musigma",
+    "100",
+    "200"
+  ],
+  [
+    "1",
+    "Quiz 1",
+    "Be the top in this quiz and get place in Musigma",
+    "100",
+    "200"
+  ],
+  [
+    "1",
+    "Quiz 1",
+    "Be the top in this quiz and get place in Musigma",
+    "100",
+    "200"
+  ]
+];
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.options = {
+      filterType: "checkbox",
+      rowsPerPage: 20,
+      elevation: 0,
+      print: false,
+      download: false,
+      responsive: "scroll",
+      pagination: false,
+      filter: false,
+      viewColumns: false,
+      selectableRows: false,
+      rowsPerPageOptions: [20, 30, 100, 200],
+      onRowClick: (rowData, rowState) => {
+        if (!this.rowSelected) {
+          this.handleRowClick(rowState.dataIndex);
+        }
+      }
+    };
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card Green>
+              <CardHeader color="warning" stats icon>
+                <CardIcon color="warning">
+                  <Icon>grade</Icon>
+                </CardIcon>
+                <p className={classes.cardCategory}>Current Score</p>
+                <h3 className={classes.cardTitle}>4.9/10</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <Danger>
+                    <Warning />
+                  </Danger>
+                  <a href="#pablo" onClick={e => e.preventDefault()}>
+                    Prepare More
+                  </a>
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="success" stats icon>
+                <CardIcon color="success">
+                  <Store />
+                </CardIcon>
+                <p className={classes.cardCategory}>Ranking</p>
+                <h3 className={classes.cardTitle}>420th</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <DateRange />
+                  Last 24 Hours
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="danger" stats icon>
+                <CardIcon color="danger">
+                  <Icon>info_outline</Icon>
+                </CardIcon>
+                <p className={classes.cardCategory}>Attempted Quizzes</p>
+                <h3 className={classes.cardTitle}>12</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <LocalOffer />
+                  Last one at 11/04/19
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="info" stats icon>
+                <CardIcon color="info">
+                  <LocalOffer />
+                </CardIcon>
+                <p className={classes.cardCategory}>Followers</p>
+                <h3 className={classes.cardTitle}>+245</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <LocalOffer />
+                  Just Updated
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+        </GridContainer>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card className={classes.root}>
+              <CardHeader color="warning">
+                <h4 className={classes.cardTitleWhite}>Attempted Quizzes</h4>
+              </CardHeader>
+              <CardBody>
+                <MUIDataTable
+                  title={""}
+                  data={datalist}
+                  columns={["No", "QuizName", "Description", "Score", "Rank"]}
+                  // options={this.options}
+                />
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
 }
 
-export default withStyles(dashboardStyle)(UserProfile);
+export default withStyles(dashboardStyle)(Results);
